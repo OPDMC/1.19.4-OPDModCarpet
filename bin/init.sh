d7a69592a6ea378ddba7789fcaea673b44a89758
TitanRGB <75297777+TitanRGB@users.noreply.github.com>
2023-05-08 21:32:13 -0400

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,10 +1,14 @@
 if [ ! -f /minecraft/start.sh ]; then
-  if [ ! -f /minecraft ]; then
+  if [ ! -d /minecraft ]; then
     mkdir /minecraft
   fi
   mv /docker_init/* /minecraft/
   sh /minecraft/start.sh
 else
+  if [ ! -d /minecraft/bin ]; then
+      mkdir /minecraft/bin
+      mv /docker_init/bin/* /minecraft/bin/
+  fi
   rm -rf /docker_init/*
   sh /minecraft/start.sh
 fi
