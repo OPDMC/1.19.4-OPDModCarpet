9bb09083dee992e0c03c65993c8fd35baa50c001
TitanRGB <75297777+TitanRGB@users.noreply.github.com>
2023-05-09 06:38:49 -0400

    Update scripts

--- bin/DockerHub_latest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p tag=" Tag to latest > "
-docker tag opdmc/1.19.4-opdmc:%tag% opdmc/1.19.4-opdmc:latest
-docker push opdmc/1.19.4-opdmc:latest
-pause
--- bin/DockerHub_sync_from_QuayIO.bat
+++ bin/DockerHub_sync_from_QuayIO.bat
@@ -1,6 +1,7 @@
 @echo off
+set repo=1.19.4-opdmc
 set /p tag=" Tag to sync > "
-docker pull quay.io/opdmc/1.19.4-opdmc:%tag%
-docker tag quay.io/opdmc/1.19.4-opdmc:%tag% opdmc/1.19.4-opdmc:%tag%
-docker push opdmc/1.19.4-opdmc:%tag%
+docker pull quay.io/%repo%:%tag%
+docker tag quay.io/%repo%:%tag% %repo%:%tag%
+docker push %repo%:%tag%
 pause
